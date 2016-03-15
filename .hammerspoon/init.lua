6e631d05b837d9807145fb160ab5f5b5d5a5d214
Alpha Chen <alpha.chen@gmail.com>
2016-03-15 13:42:43 -0700

    [hammerspoon] Workaround for <esc> in Safari

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,13 +3,19 @@ local mash = {"cmd", "alt", "ctrl", "shift"}
 
 -- Application shortcuts
 
-hs.hotkey.bind({}, "escape", function()
+local wf = hs.window.filter
+local safari_esc = hs.hotkey.new({}, "escape", function() end, function()
   if current_app():name() == "Safari" then
     hs.eventtap.keyStroke({"alt"}, "escape")
-  else
-    hs.eventtap.keyStroke({}, "escape")
   end
 end)
+
+wf.new("Safari"):subscribe(wf.windowFocused, function()
+  safari_esc:enable()
+end):subscribe(wf.windowUnfocused, function()
+  safari_esc:disable()
+end)
+
 
 -- hs.hotkey.bind({"cmd", "shift"}, "[", function()
 --   current_app():selectMenuItem({"Window", "Select Previous Tab"})
