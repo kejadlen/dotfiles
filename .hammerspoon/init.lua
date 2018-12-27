699f4bb41194596ab71dcf53d7566dae07f9a2ad
Alpha Chen <alpha.chen@gmail.com>
2018-12-27 10:19:19 -0800

    [pivotal][hammerspoon][chunkwm]

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,76 +1,30 @@
 local mash = {"cmd", "alt", "ctrl"}
 local smash = {"cmd", "alt", "ctrl", "shift"}
 
-function current_app()
-  return hs.application.frontmostApplication()
-end
-
--- Application shortcuts
-
--- local safari_esc = hs.hotkey.new({}, "escape", function() end, function()
---   if current_app():name() == "Safari" then
---     hs.eventtap.keyStroke({"alt"}, "escape")
---   end
--- end)
-
--- local safari_close_tab = hs.hotkey.new({"cmd"}, "w", function() end, function()
---   if current_app():name() == "Safari" then
---     current_app():selectMenuItem({"File", "Close Tab"})
---   end
--- end)
-
--- local wf = hs.window.filter
--- wf.new("Safari"):subscribe(wf.windowFocused, function()
---   -- safari_esc:enable()
---   safari_close_tab:enable()
--- end):subscribe(wf.windowUnfocused, function()
---   -- safari_esc:disable()
---   safari_close_tab:disable()
--- end)
-
--- hs.hotkey.bind({"cmd", "shift"}, "[", function()
---   current_app():selectMenuItem({"Window", "Select Previous Tab"})
--- end)
-
--- hs.hotkey.bind({"cmd", "shift"}, "]", function()
---   current_app():selectMenuItem({"Window", "Select Next Tab"})
--- end)
-
 -- Window management
 
-local throw_left = function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+function chunkc (args)
+  hs.task.new("/usr/local/bin/chunkc", nil, args)
+    :start()
 end
 
-local throw_right = function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+hs.hotkey.bind(mash, "left", function()
+  chunkc({"tiling::window", "--warp", "west"})
+end)
+hs.hotkey.bind(mash, "right", function()
+  chunkc({"tiling::window", "--warp", "east"})
+end)
+hs.hotkey.bind(mash, "up", function()
+  chunkc({"tiling::window", "--warp", "north"})
+end)
+hs.hotkey.bind(mash, "down", function()
+  chunkc({"tiling::window", "--warp", "south"})
+end)
+hs.hotkey.bind(mash, "z", function()
+  chunkc({"tiling::window", "--toggle", "fullscreen"})
+end)
 
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end
-
-hs.hotkey.bind(mash, "h", throw_left)
-hs.hotkey.bind(mash, "left", throw_left)
-hs.hotkey.bind(mash, "l", throw_right)
-hs.hotkey.bind(mash, "right", throw_right)
-
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", function()
-  hs.reload()
-end):start()
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
 
 hs.alert.show("Config loaded")
--- .vim/after/ftplugin/ruby.vim
+++ .vim/after/ftplugin/ruby.vim
@@ -1,5 +1,4 @@
 " https://github.com/vim-ruby/vim-ruby/issues/243
 set regexpengine=1
 
-highlight rubyDefine ctermbg=235
 set foldmethod=syntax
