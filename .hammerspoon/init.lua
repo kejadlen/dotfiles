8953f59f382563f271c7a58c09e0b28625ee0503
Alpha Chen <alpha@kejadlen.dev>
2020-09-29 10:04:49 -0700

    [hammerspoon] handle different Lua versions
    See https://github.com/Hammerspoon/hammerspoon/issues/2473#issuecomment-695873284

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,4 +1,16 @@
-fennel = require("fennel")
+local lVer = _VERSION:match("Lua (.+)$")
+-- local luarocks = hs.execute("which luarocks"):gsub("\n", "")
+local luarocks = "/usr/local/bin/luarocks"
+if #luarocks > 0 then
+  package.path = package.path .. ";" .. hs.execute(
+    luarocks .. " --lua-version " .. lVer .. " path --lr-path"
+  ):gsub("\n", "")
+  package.cpath = package.cpath .. ";" .. hs.execute(
+    luarocks .. " --lua-version " .. lVer .. " path --lr-cpath"
+  ):gsub("\n", "")
+end
+
+fennel = require "fennel"
 table.insert(package.loaders or package.searchers, fennel.searcher)
 
 fennel.dofile("init.fnl", { allowedGlobals = false })
