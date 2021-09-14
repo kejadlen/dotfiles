ce69de1f45aa68bb09b68ae9376e47be8c9adaea
Alpha Chen <alpha@kejadlen.dev>
2021-09-14 10:18:11 -0700

    [meta] delete ramble-hard k8s experiment

--- ramble-hard/outputs.tf
+++
@@ -1,5 +0,0 @@
-resource "local_file" "kubeconfig" {
-  depends_on   = [linode_lke_cluster.lke_cluster]
-  filename     = "kube.config.private"
-  content      = base64decode(linode_lke_cluster.lke_cluster.kubeconfig)
-}
