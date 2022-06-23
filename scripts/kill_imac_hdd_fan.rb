fc9ef8fafc33c658655c71e6f93ce5f5c374d493
Alpha Chen <alpha@kejadlen.dev>
2022-06-22 20:14:35 -0700

    [scripts] rm

--- scripts/kill_imac_hdd_fan.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-
-fan_speed = (ARGV.shift || 1100).to_i
-
-smc = '~/Applications/smcFanControl.app/Contents/Resources/smc'
-fan_speed = (fan_speed << 2).to_s(16)
-`#{smc} -k F1Mx -w #{fan_speed}`
