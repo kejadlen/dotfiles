36544f8606b525437316bfc5819bc28fa58dcee3
Alpha Chen <alpha@kejadlen.dev>
2024-06-07 16:44:53 -0700

    sketchybar

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -8,21 +8,21 @@ if [ "$PERCENTAGE" = "" ]; then
 fi
 
 case "${PERCENTAGE}" in
-  9[0-9]|100) ICON=""
+  9[0-9]|100) ICON=􀛨
   ;;
-  [6-8][0-9]) ICON=""
+  [6-8][0-9]) ICON=􀺸
   ;;
-  [3-5][0-9]) ICON=""
+  [3-5][0-9]) ICON=􀺶
   ;;
-  [1-2][0-9]) ICON=""
+  [1-2][0-9]) ICON=􀛩
   ;;
-  *) ICON=""
+  *) ICON=􀛪
 esac
 
 if [[ "$CHARGING" != "" ]]; then
-  ICON=""
+  ICON=􀢋
 fi
 
 # The item invoking this script (name $NAME) will get its icon and label
 # updated with the current battery status
-sketchybar --set "$NAME" icon="$ICON" label="${PERCENTAGE}%"
+sketchybar --set "$NAME" icon="$ICON" label="$PERCENTAGE%"
