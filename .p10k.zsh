ec5b2063788ba4c68ec43b741e9782ae3559a460
Alpha Chen <alpha.chen@brilliant.org>
2024-08-23 16:52:37 -0700

    p10k context

--- .p10k.zsh
+++ .p10k.zsh
@@ -37,7 +37,7 @@
     vcs                     # git status
     # =========================[ Line #2 ]=========================
     newline                 # \n
-    context
+    context                 # user@hostname
     prompt_char             # prompt symbol
   )
 
@@ -86,7 +86,6 @@
     gcloud                  # google cloud cli account and project (https://cloud.google.com/)
     google_app_cred         # google application credentials (https://cloud.google.com/docs/authentication/production)
     toolbox                 # toolbox name (https://github.com/containers/toolbox)
-    context                 # user@hostname
     nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
     ranger                  # ranger shell (https://github.com/ranger/ranger)
     nnn                     # nnn shell (https://github.com/jarun/nnn)
