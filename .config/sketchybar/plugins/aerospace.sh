ca3a098999d7ca3cb426d0a738f5fd75f96120b8
Alpha Chen <alpha@kejadlen.dev>
2024-10-10 21:03:39 -0700

    faster aerospace + sketchybar

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -3,9 +3,7 @@
 # Original source:
 #   https://nikitabobko.github.io/AeroSpace/goodness#show-aerospace-workspaces-in-sketchybar
 
-workspaces=("${(@f)$(aerospace list-workspaces --monitor $1)}")
-
-if (($workspaces[(Ie)$FOCUSED_WORKSPACE])); then
+if (($argv[(Ie)$FOCUSED_WORKSPACE])); then
     sketchybar --set "$NAME" label="$FOCUSED_WORKSPACE" background.drawing=on
 else
     sketchybar --set "$NAME" label="$FOCUSED_WORKSPACE" background.drawing=off
