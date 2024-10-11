7759036795f79a98b65b96d8c7928cef481d29cf
Alpha Chen <alpha.chen@brilliant.org>
2024-10-11 10:00:10 -0700

    use bash for sketchybar aerospace plugin

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 # Original source:
 #   https://nikitabobko.github.io/AeroSpace/goodness#show-aerospace-workspaces-in-sketchybar
 
-if (($argv[(Ie)$FOCUSED_WORKSPACE])); then
+# https://stackoverflow.com/a/47541882
+if printf '%s\0' "${argv[@]}" | grep -F -x -z -- "$FOCUSED_WORKSPACE"; then
     sketchybar --set "$NAME" label="$FOCUSED_WORKSPACE" background.drawing=on
 else
     sketchybar --set "$NAME" label="$FOCUSED_WORKSPACE" background.drawing=off
