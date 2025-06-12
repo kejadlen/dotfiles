093f21381c6029324313a899a77aa9470e369a4d
Alpha Chen <alpha@kejadlen.dev>
2025-06-12 09:10:02 -0700

    [sketchybar] reset aerospace

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -3,9 +3,6 @@
 # Original source:
 #   https://nikitabobko.github.io/AeroSpace/goodness#show-aerospace-workspaces-in-sketchybar
 
-# https://stackoverflow.com/a/47541882
-if printf '%s\0' "${argv[@]}" | grep -F -x -z -- "$FOCUSED_WORKSPACE"; then
-    sketchybar --set "$NAME" label="$FOCUSED_WORKSPACE" background.drawing=on
-else
-    sketchybar --set "$NAME" label="$FOCUSED_WORKSPACE" background.drawing=off
-fi
+# Update the current workspace label
+CURRENT_WORKSPACE=$(aerospace list-workspaces --focused)
+sketchybar --set $NAME label="$CURRENT_WORKSPACE"
